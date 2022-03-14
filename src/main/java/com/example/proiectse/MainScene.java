9956335027ef7ebaf7b71574cc555f958675e202
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2022-03-14 15:43:21 +0200

    Added structure

---
+++ src/main/java/com/example/proiectse/MainScene.java
@@ -0,0 +1,20 @@
+package com.example.proiectse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainScene extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+}
