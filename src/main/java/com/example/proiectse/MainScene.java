d2ef0b3817336a02514f54119297c58785419507
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2022-03-19 14:51:31 +0200

    added main scene

--- src/main/java/com/example/proiectse/MainScene.java
+++ src/main/java/com/example/proiectse/MainScene.java
@@ -15,6 +15,11 @@ public class MainScene extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainscene-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/com/example/proiectse/MainSceneController.java
+++ src/main/java/com/example/proiectse/MainSceneController.java
@@ -1,6 +1,40 @@
 package com.example.proiectse;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
 public class MainSceneController {
 
+    ObservableList<String> ageList = FXCollections.observableArrayList("18 - 35","36 - 50", "51+");
+    ObservableList<String> tempList = FXCollections.observableArrayList("Warm","Cold","Temperate","Irrelevant");
+    ObservableList<String> activList = FXCollections.observableArrayList("Relaxing","Recreative","Sport","Irrelevant");
+    ObservableList<String> budgetList = FXCollections.observableArrayList("Low","Medium","High");
+    ObservableList<String> locationList = FXCollections
+            .observableArrayList("Inside the country","Inside the continent","Everywhere in the world");
+
+    @FXML
+    private ChoiceBox ageChoiceBox;
+    @FXML
+    private ChoiceBox tempChoiceBox;
+    @FXML
+    private ChoiceBox activChoiseBox;
+    @FXML
+    private ChoiceBox budgetChoiseBox;
+    @FXML
+    private ChoiceBox locationChoiseBox;
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private void initialize(){
+        ageChoiceBox.setItems(ageList);
+        tempChoiceBox.setItems(tempList);
+        activChoiseBox.setItems(activList);
+        budgetChoiseBox.setItems(budgetList);;
+        locationChoiseBox.setItems(locationList);
+    }
 
 }
